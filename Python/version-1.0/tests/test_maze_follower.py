1d466642c16ca556b93f7481f9cf830d7e633aaf
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-20 20:32:53 -0500

    Third test failing.

--- Python/version-1.0/tests/test_maze_follower.py
+++ Python/version-1.0/tests/test_maze_follower.py
@@ -21,3 +21,8 @@ def test_given_i_have_received_a_board_that_is_missing_an_end_position_when_eval
         assert(False)
     except ValueError:
         return
+
+def test_given_i_have_received_a_valid_board_when_stepping_then_no_error_is_thrown():
+    maze = "SE"
+    result = step(maze)
+    assert(False)
