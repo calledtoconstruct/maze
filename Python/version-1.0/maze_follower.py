0fba86fa15dc9b62632d61ca9a0718689a9d3cde
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-21 13:51:51 -0500

    Refactored maze to be array of string.

--- Python/version-1.0/maze_follower.py
+++ Python/version-1.0/maze_follower.py
@@ -3,9 +3,18 @@
 def test():
     return 100
 
-def step(maze):
-    if "S" not in maze:
+def step(maze, x, y):
+    start = 0
+    end = 0
+    for line in maze:
+        if "S" in line:
+            start = start + 1
+        if "E" in line:
+            end = end + 1
+    if start == 0 or end == 0:
         raise ValueError()
-    if "E" not in maze:
-        raise ValueError()
-    return
+    y = 0       
+    for line in maze:
+        y = y + 1
+        
+    return "S", 1, 0
--- Python/version-1.0/tests/test_maze_follower.py
+++ Python/version-1.0/tests/test_maze_follower.py
@@ -7,22 +7,47 @@ def test_test():
     assert(result == 100)
 
 def test_given_i_have_received_a_board_that_is_missing_a_start_position_when_evaluating_then_an_exception_is_thrown():
-    maze = "E"
+    maze = ["E"]
     try:
-        result = step(maze)
+        result, x, y = step(maze, -1, -1)
         assert(False)
     except ValueError:
         return
 
 def test_given_i_have_received_a_board_that_is_missing_an_end_position_when_evaluating_then_an_exception_is_thrown():
-    maze = "S"
+    maze = ["S"]
     try:
-        result = step(maze)
+        result, x, y = step(maze, -1, -1)
         assert(False)
     except ValueError:
         return
 
 def test_given_i_have_received_a_valid_board_when_stepping_then_no_error_is_thrown():
-    maze = "SE"
-    result = step(maze)
-    assert(False)
+    maze = ["SE"]
+    result, x, y = step(maze, -1, -1)
+    assert(True)
+
+def test_given_i_have_received_a_valid_board_with_multiple_lines_when_stepping_then_no_error_is_thrown():
+    maze = ["S","E"]
+    result, x, y = step(maze, -1, -1)
+    assert(True)
+
+def test_given_an_unnavigated_board_when_stepping_then_the_start_position_is_returned():
+    maze = [
+        "WS",
+        "WE"
+    ]
+    result, x, y = step(maze, -1, -1)
+    assert(result == "S")
+    assert(x == 1)
+    assert(y == 0)
+
+def test_given_i_am_at_a_known_location_when_stepping_then_the_empty_space_position_is_returned():
+    maze = [
+        "WS",
+        "W E"
+    ]
+    result, x, y = step(maze, 1, 0)
+    assert(result == " ")
+    assert(x == 1)
+    assert(y == 1)
