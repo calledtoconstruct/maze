a85881234f63f7f3192b3ff4ee7155da22cdbe11
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-23 14:30:21 -0500

    Test 12 succeeded, Victory!

--- Python/version-1.0/maze_follower.py
+++ Python/version-1.0/maze_follower.py
@@ -4,26 +4,29 @@ def test():
     return 100
 
 def step(maze, cx = -1, cy = -1):
-    start = 0
-    end = 0
-    for line in maze:
-        if "S" in line:
-            start = start + 1
-        if "E" in line:
-            end = end + 1
-    if start == 0 or end == 0:
-        raise ValueError()
     if cx == -1 or cy == -1:
+        start = 0
+        end = 0
+        for line in maze:
+            if "S" in line:
+                start = start + 1
+            if "E" in line:
+                end = end + 1
+        if start == 0 or end == 0:
+            raise ValueError()
         for y, line in enumerate(maze):
             for x, current in enumerate(line):
                 if current == 'S':
                     return current, x, y
     for y, line in enumerate(maze):
         for x, current in enumerate(line):
-            if current == ' ':
-                if x in range(cx-1, 3):
-                    if y in range(cy-1, 3):
-                        maze[cy] = maze[cy][0:cx] + '*' + maze[cy][cx+1:]
-                        maze[y] = maze[y][0:x] + 'C' + maze[y][x+1:]
-                        return current, x, y
+            if cx == x or cy == y:
+                if current == 'E':
+                    return "Victory!", x, y
+                if current == ' ':
+                    if x in range(cx-1, 3):
+                        if y in range(cy-1, 3):
+                            maze[cy] = maze[cy][0:cx] + '*' + maze[cy][cx+1:]
+                            maze[y] = maze[y][0:x] + 'C' + maze[y][x+1:]
+                            return current, x, y
     raise ValueError
--- Python/version-1.0/tests/test_maze_follower.py
+++ Python/version-1.0/tests/test_maze_follower.py
@@ -9,7 +9,7 @@ def test_test():
 def test_given_i_have_received_a_board_that_is_missing_a_start_position_when_evaluating_then_an_exception_is_thrown():
     maze = ["E"]
     try:
-        result, x, y = step(maze, -1, -1)
+        result, x, y = step(maze)
         assert(False)
     except ValueError:
         return
@@ -17,19 +17,19 @@ def test_given_i_have_received_a_board_that_is_missing_a_start_position_when_eva
 def test_given_i_have_received_a_board_that_is_missing_an_end_position_when_evaluating_then_an_exception_is_thrown():
     maze = ["S"]
     try:
-        result, x, y = step(maze, -1, -1)
+        result, x, y = step(maze)
         assert(False)
     except ValueError:
         return
 
 def test_given_i_have_received_a_valid_board_when_stepping_then_no_error_is_thrown():
     maze = ["SE"]
-    result, x, y = step(maze, -1, -1)
+    result, x, y = step(maze)
     assert(True)
 
 def test_given_i_have_received_a_valid_board_with_multiple_lines_when_stepping_then_no_error_is_thrown():
     maze = ["S","E"]
-    result, x, y = step(maze, -1, -1)
+    result, x, y = step(maze)
     assert(True)
 
 def test_given_an_unnavigated_board_when_stepping_then_the_start_position_is_returned():
@@ -37,7 +37,7 @@ def test_given_an_unnavigated_board_when_stepping_then_the_start_position_is_ret
         "WS",
         "WE"
     ]
-    result, x, y = step(maze, -1, -1)
+    result, x, y = step(maze)
     assert(result == "S")
     assert(x == 1)
     assert(y == 0)
@@ -52,7 +52,7 @@ def test_given_i_am_at_a_known_location_when_stepping_then_the_position_of_the_e
     assert(x == 1)
     assert(y == 1)
 
-def test_given_i_am_at_a_known_location_when_stepping_then_the_position_of_the_empty_space_is_returned():
+def test_given_i_am_at_a_known_location_when_stepping_then_an_error_is_raised():
     maze = [
         "WSW",
         "WWE"
@@ -84,3 +84,13 @@ def test_given_i_am_at_a_known_location_when_stepping_then_the_location_i_move_t
     ]
     result, x, y = step(maze, 1, 0)
     assert(maze[y][x] == 'C')
+
+def test_given_i_am_at_a_known_location_and_the_endpoint_is_adjacent_when_stepping_then_victory_and_the_position_of_the_endpoint_is_returned():
+    maze = [
+        "W*W",
+        "WCE"
+    ]
+    result, x, y = step(maze, 1, 1)
+    assert(result == "Victory!")
+    assert(x == 2)
+    assert(y == 1)
