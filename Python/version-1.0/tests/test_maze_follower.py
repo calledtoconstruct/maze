f085600050334074eff634c4f2ad19fd11c86a17
Joseph Woolley <joe@calledtoconstruct.net>
2018-12-26 10:52:34 -0500

    Test for dead-end fails.

--- Python/version-1.0/tests/test_maze_follower.py
+++ Python/version-1.0/tests/test_maze_follower.py
@@ -49,7 +49,7 @@ def test_given_i_am_at_a_known_location_when_stepping_then_the_position_of_the_e
         assert(x == ex)
         assert(y == ey)
 
-def test_given_i_am_at_a_known_location_when_stepping_then_an_error_is_raised():
+def test_given_i_am_at_a_known_location_and_no_option_exists_when_stepping_then_an_error_is_raised():
     maze = [
         "WSW",
         "WWE"
@@ -137,3 +137,12 @@ def test_given_i_am_at_a_known_location_and_two_options_exist_when_stepping_then
         assert(y == ny)
         assert(len(options) == l)
         assert(options[0] == (fox, foy))
+
+def test_given_i_am_at_a_known_location_and_no_options_exist_when_stepping_then_dead_end_is_returned():
+    maze = [
+        "W*W",
+        "WCW",
+        "WWW"
+    ]
+    result, x, y, options = step(maze, 1, 1)
+    assert(result == "D")
